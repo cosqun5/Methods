661f7ffc594cda0d301f19c6b72298c0943a381d
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-04-05 11:34:37 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿#region Task1
+
+
+//Console.Write("Enter Number :");
+//int number = int.Parse(Console.ReadLine());
+//Print(number);
+//void Print(int number)
+//{
+//    if (number % 2 == 0)
+//    {
+//        Console.WriteLine(number + " " + "Eded cutdur");
+//return;
+//    }
+//        Console.WriteLine(number + " " + "Eded tekdir");
+//}
+#endregion
+
+#region Task3
+
+
+//Console.Write("Enter the first number:");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.Write("Enter the second number:");
+//int num2 = int.Parse(Console.ReadLine());
+//Print( Sum(num1, num2));
+////Print( Multiplication(num1, num2));
+////Print( Division(num1, num2));
+////Print(Difference(num1, num2));
+
+//int Sum (int num1, int num2)
+//{
+//    return num1 + num2; 
+//}
+//int Multiplication(int num1, int num2)
+//{
+//    return num1 * num2;
+//}
+
+//int Difference (int num1, int num2)
+//{
+//    return num1 - num2;
+//}
+//int Division (int num1, int num2)
+//{
+//    return num1 / num2;
+//}
+//void Print (int Total)
+//{
+//    Console.WriteLine(Total);
+//}
+#endregion
+
+
+
